276658bc5a1d21910a2bf9cdd66c48b657f77a3d
Nevin George <nev1n>
2018-07-12 09:22:44 +0200

    Muxes added, without testbenches

--- src/components/Alu1bit.vhd
+++ src/components/Alu1bit.vhd
@@ -3,10 +3,10 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity ALU1BIT is
+entity ALU1bit is
   port (
     A         : in std_logic;
     B         : in std_logic;
     Result    : out std_logic
   );
-end ALU1BIT;
+end ALU1bit;
--- src/testbenches_components/Adder_TB.vhd
+++ src/testbenches_components/Adder_TB.vhd
@@ -1,3 +1,5 @@
+-- Test Bench for Adder Unit
+
 library ieee;
 use ieee.std_logic_1164.all;
 
