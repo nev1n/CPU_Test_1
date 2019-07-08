8e93bfdeee2df112d30c586ae8e7c5c43f7baa05
Nevin <nevin@outlook.com>
2019-07-08 10:47:08 +0200

    Update SlackMonitor.vhd

--- src/components/SlackMonitor.vhd
+++ src/components/SlackMonitor.vhd
@@ -5,10 +5,10 @@ use ieee.std_logic_1164.all;
 
 entity SlackMonitor is 
 	port (
-		clk				:	in std_logic;
+		clk			:	in std_logic;
 		reset			:	in std_logic;
 		data_in			:	in std_logic;
-		captureEnable	:	in std_logic;
+		captureEnable		:	in std_logic;
 		data_out		:	in std_logic;
 		slack_val		:	out std_logic_vector(3 downto 0);
 		warning			:	out std_logic
@@ -24,7 +24,7 @@ begin
 	
 	
 	Level1Dff	:	entity work.Dff port map (captureEnable, reset, data_in, slack_val_war);
-	slack_val(0) <= slack_val_war;
+	slack_val(0)	<= 	slack_val_war;
 	
 	Level2delay	:	entity work.DelayElement port map(captureEnable, delay2);
 	Level2Dff	:	entity work.Dff port map (delay2, reset, data_in, slack_val(1));
@@ -37,6 +37,6 @@ begin
 	
 	 
 	-- warning generation with first slack val. Can this be dynamic?
-	warning <= data_out xor slack_val_war;
+	warning 	<= 	data_out xor slack_val_war;
 	
 end architecture behaviour_1;
